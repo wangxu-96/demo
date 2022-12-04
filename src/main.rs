9c30a3046a294d66a4c10d613b310a5681ffd25f
leisure <leisure960722@gmail.com>
2022-12-04 17:48:59 +0800

    file match

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::collections::HashMap;
+use std::fs::File;
+
 fn main() {
+    let f = File::open("hello.txt");
+    match f {
+        Ok(f) => f,
+        Err(e) => match e.kind() {
+            std::io::ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(f) => f,
+                Err(e) => panic!("Error creating file: {}", e)
+            },
+            _ => panic!("file error"),
+        },
+    };
     println!("Hello, world!");
 }
